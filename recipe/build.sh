f212767a139ad750e4eb4df3b8cc379fa3f7aef6
Ray Donnelly <mingw.android@gmail.com>
2017-12-24 22:37:21 +0000

    Update to 3.21.0 and enable more features

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,6 +21,14 @@ fi
 
 [[ "$GPL_ok" = 1 ]] && READLINE="--enable-readline --disable-editline" || READLINE="--disable-readline --enable-editline"
 
+export CPPFLAGS="${CPPFLAGS} -DSQLITE_ENABLE_COLUMN_METADATA=1 \
+                             -DSQLITE_ENABLE_UNLOCK_NOTIFY \
+                             -DSQLITE_ENABLE_DBSTAT_VTAB=1 \
+                             -DSQLITE_ENABLE_FTS3_TOKENIZER=1 \
+                             -DSQLITE_SECURE_DELETE \
+                             -DSQLITE_MAX_VARIABLE_NUMBER=250000 \
+                             -DSQLITE_MAX_EXPR_DEPTH=10000"
+
 ./configure --prefix=${PREFIX}   \
             --build=${BUILD}     \
             --host=${HOST}       \
